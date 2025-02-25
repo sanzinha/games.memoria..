d18e4471594e9977616a3780f70b40f9797b90ea
samara <126591429+sanzinha@users.noreply.github.com>
2025-02-25 10:54:20 -0300

    Add files via upload

--- memoria.py
+++ memoria.py
@@ -1,28 +1,34 @@
-print ('***************')
+print ('*********************************************')
 print ('Bem vindo ao memoria, seu jogo de adivinhação')
-print ('***************')
-
-chute = int(input('Digite seu número: '))
+print ('*********************************************')
 
 numero_secreto = 7
-acertou = chute == numero_secreto
-chute_maior = chute > numero_secreto
-chute_menor = chute < numero_secreto
+total_de_tentativas = 4
+rodada = 1
+
+while (rodada <= total_de_tentativas):
+    print(f'tentativas {rodada} de {total_de_tentativas}')
+    chute = int(input('digite o seu numero'))
+
+    acertou = chute == numero_secreto
+    chute_maior = chute > numero_secreto
+    chute_menor = chute < numero_secreto
 
 
 
-print (f'Você  digitou {chute} ')
+    print (f'Você  digitou {chute} ')
 
-if(acertou):
-    print('Você acertou!')
-else:
-    if(chute_maior):
-        print('Você errou! Seu número foi maior do que o número secreto')
-    elif(chute_menor):
-        print('memoria fraca <3, seu número é menor do que o número secreto.')
-
-    
+    if(acertou):
+        print('Você acertou!')
+    else:
+        if(chute_maior):
+            print('Você errou! Seu número foi maior do que o número secreto')
+        elif(chute_menor):
+            print('memoria fraca <3, seu número é menor do que o número secreto.')
+rodada = rodada + 1
         
+        
+            
 print('@@@@@@@@@@@')
 print('Fim de jogo')
 print('@@@@@@@@@@@')
